1acee5dbae336a8681226780c22d4b651a2c2569
Radovan Myslik <rada.myslik@gmail.com>
2025-11-20 21:45:49 +0100

    Fixed process_manager filesystem handle

--- sources/userspace/logger_task/main.cpp
+++ sources/userspace/logger_task/main.cpp
@@ -6,74 +6,72 @@
 
 /**
  * Logger task
- * 
+ *
  * Prijima vsechny udalosti od ostatnich tasku a oznamuje je skrz UART hostiteli
  **/
 
-static void fputs(uint32_t file, const char* string)
+static void fputs(uint32_t file, const char *string)
 {
-	write(file, string, strlen(string));
+    write(file, string, strlen(string));
+    write(file, "\n", 1);
 }
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-	// char buf[16];
-	// char tickbuf[16];
-	// bzero(buf, 16);
-	// bzero(tickbuf, 16);
+    // char buf[16];
+    // char tickbuf[16];
+    // bzero(buf, 16);
+    // bzero(tickbuf, 16);
 
-	// uint32_t last_tick = 0;
+    // uint32_t last_tick = 0;
 
-	// uint32_t logpipe = pipe("log", 32);
+    // uint32_t logpipe = pipe("log", 32);
 
-	// while (true)
-	// {
-	// 	wait(logpipe, 1, 0x1000);
+    // while (true)
+    // {
+    // 	wait(logpipe, 1, 0x1000);
 
-	// 	uint32_t v = read(logpipe, buf, 15);
-	// 	if (v > 0)
-	// 	{
-	// 		buf[v] = '\0';
-	// 		fputs(uart_file, "\r\n[ ");
-	// 		uint32_t tick = get_tick_count();
-	// 		itoa(tick, tickbuf, 16);
-	// 		fputs(uart_file, tickbuf);
-	// 		fputs(uart_file, "]: ");
-	// 		fputs(uart_file, buf);
-	// 	}
-	// }
+    // 	uint32_t v = read(logpipe, buf, 15);
+    // 	if (v > 0)
+    // 	{
+    // 		buf[v] = '\0';
+    // 		fputs(uart_file, "\r\n[ ");
+    // 		uint32_t tick = get_tick_count();
+    // 		itoa(tick, tickbuf, 16);
+    // 		fputs(uart_file, tickbuf);
+    // 		fputs(uart_file, "]: ");
+    // 		fputs(uart_file, buf);
+    // 	}
+    // }
 
     uint32_t uart_file = open("DEV:uart/0", NFile_Open_Mode::Write_Only);
 
-	TUART_IOCtl_Params params;
-	params.baud_rate = NUART_Baud_Rate::BR_115200;
-	params.char_length = NUART_Char_Length::Char_8;
-	ioctl(uart_file, NIOCtl_Operation::Set_Params, &params);
+    TUART_IOCtl_Params params;
+    params.baud_rate = NUART_Baud_Rate::BR_115200;
+    params.char_length = NUART_Char_Length::Char_8;
+    ioctl(uart_file, NIOCtl_Operation::Set_Params, &params);
 
-	fputs(uart_file, "UART Task A starting!");
+    fputs(uart_file, "UART Task A starting!");
 
     uint32_t memfile = open("SYS:shm/my_shared_memory", NFile_Open_Mode::Read_Write);
     char *mem = mmap(0x100000, memfile);
     mem[10] = 'a';
-    mutex_t mutex = mutex_create("shm_mem_mutex");
 
+    char c[2];
+    c[1] = '\0';
     while (true)
     {
-        char c[2];
-        c[1] = '\0';
-        mutex_lock(mutex);
         c[0] = mem[10];
         if (c[0] == 'a')
         {
             fputs(uart_file, c);
             mem[10] = 'b';
         }
-        mutex_unlock(mutex);
     }
 
     // // free(mem);
     close(memfile);
-    mutex_destroy(mutex);
+    close(uart_file);
 
     return 0;
 }
--- sources/userspace/oled_task/main.cpp
+++ sources/userspace/oled_task/main.cpp
@@ -11,7 +11,7 @@
 
 /**
  * Displejovy task
- * 
+ *
  * Zobrazuje hlasky na OLED displeji, a pokud prijde udalost od jinych tasku, zobrazi neco relevantniho k nim
  **/
 
@@ -23,59 +23,55 @@
 // 	"Old MacDonald had a farm, EIGRP",
 // };
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
     // uint32_t uart_file = open("DEV:uart/0", NFile_Open_Mode::Write_Only);
 
-	// TUART_IOCtl_Params params;
-	// params.baud_rate = NUART_Baud_Rate::BR_115200;
-	// params.char_length = NUART_Char_Length::Char_8;
-	// ioctl(uart_file, NIOCtl_Operation::Set_Params, &params);
+    // TUART_IOCtl_Params params;
+    // params.baud_rate = NUART_Baud_Rate::BR_115200;
+    // params.char_length = NUART_Char_Length::Char_8;
+    // ioctl(uart_file, NIOCtl_Operation::Set_Params, &params);
 
-	// fputs(uart_file, "UART Task B starting!");
-	// COLED_Display disp("DEV:oled");
-	// disp.Clear(false);
-	// disp.Put_String(10, 10, "KIV-RTOS init...");
-	// disp.Flip();
+    // fputs(uart_file, "UART Task B starting!");
+    // COLED_Display disp("DEV:oled");
+    // disp.Clear(false);
+    // disp.Put_String(10, 10, "KIV-RTOS init...");
+    // disp.Flip();
 
-	// uint32_t trng_file = open("DEV:trng", NFile_Open_Mode::Read_Only);
-	// uint32_t num = 0;
+    // uint32_t trng_file = open("DEV:trng", NFile_Open_Mode::Read_Only);
+    // uint32_t num = 0;
 
-	// sleep(0x800, 0x800);
+    // sleep(0x800, 0x800);
 
-	// while (true)
-	// {
-	// 	// ziskame si nahodne cislo a vybereme podle toho zpravu
-	// 	read(trng_file, reinterpret_cast<char*>(&num), sizeof(num));
-	// 	const char* msg = messages[num % (sizeof(messages) / sizeof(const char*))];
+    // while (true)
+    // {
+    // 	// ziskame si nahodne cislo a vybereme podle toho zpravu
+    // 	read(trng_file, reinterpret_cast<char*>(&num), sizeof(num));
+    // 	const char* msg = messages[num % (sizeof(messages) / sizeof(const char*))];
 
-	// 	disp.Clear(false);
-	// 	disp.Put_String(0, 0, msg);
-	// 	disp.Flip();
+    // 	disp.Clear(false);
+    // 	disp.Put_String(0, 0, msg);
+    // 	disp.Flip();
 
-	// 	sleep(0x4000, 0x800); // TODO: z tohohle bude casem cekani na podminkove promenne (na eventu) s timeoutem
-	// }
+    // 	sleep(0x4000, 0x800); // TODO: z tohohle bude casem cekani na podminkove promenne (na eventu) s timeoutem
+    // }
 
     uint32_t memfile = open("SYS:shm/my_shared_memory", NFile_Open_Mode::Read_Write);
     char *mem = mmap(0x100000, memfile);
-    mutex_t mutex = mutex_create("shm_mem_mutex");
 
+    char c[2];
+    c[1] = '\0';
     while (true)
     {
-        char c[2];
-        c[1] = '\0';
-        mutex_lock(mutex);
         c[0] = mem[10];
         if (c[0] == 'b')
         {
             mem[10] = 'a';
         }
-        mutex_unlock(mutex);
     }
 
     // free(mem);
     close(memfile);
-    mutex_destroy(mutex);
 
     return 0;
 }
