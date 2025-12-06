9abf460230dc9a13535ed980309ead1c6e2f088e
Radovan Myslik <rada.myslik@gmail.com>
2025-12-06 17:34:31 +0100

    Returned tasks back to original

--- sources/userspace/counter_task/main.cpp
+++ sources/userspace/counter_task/main.cpp
@@ -15,55 +15,40 @@
 
 int main(int argc, char** argv)
 {
-	// uint32_t display_file = open("DEV:segd", NFile_Open_Mode::Write_Only);
-	// uint32_t switch1_file = open("DEV:gpio/4", NFile_Open_Mode::Read_Only);
-	// uint32_t switch2_file = open("DEV:gpio/17", NFile_Open_Mode::Read_Only);
+	uint32_t display_file = open("DEV:segd", NFile_Open_Mode::Write_Only);
+	uint32_t switch1_file = open("DEV:gpio/4", NFile_Open_Mode::Read_Only);
+	uint32_t switch2_file = open("DEV:gpio/17", NFile_Open_Mode::Read_Only);
 
-	// unsigned int counter = 0;
-	// bool fast = false;
-	// bool ascending = true;
+	unsigned int counter = 0;
+	bool fast = false;
+	bool ascending = true;
 
-	// set_task_deadline(fast ? 0x1000 : 0x2800);
+	set_task_deadline(fast ? 0x1000 : 0x2800);
 
-	// while (true)
-	// {
-	// 	char tmp = '0';
+	while (true)
+	{
+		char tmp = '0';
 
-	// 	read(switch1_file, &tmp, 1);
-	// 	ascending = (tmp == '1');
+		read(switch1_file, &tmp, 1);
+		ascending = (tmp == '1');
 
-	// 	read(switch2_file, &tmp, 1);
-	// 	fast = (tmp == '1');
+		read(switch2_file, &tmp, 1);
+		fast = (tmp == '1');
 
-	// 	if (ascending)
-	// 		counter++;
-	// 	else
-	// 		counter--;
+		if (ascending)
+			counter++;
+		else
+			counter--;
 
-	// 	tmp = '0' + (counter % 10);
-	// 	write(display_file, &tmp, 1);
+		tmp = '0' + (counter % 10);
+		write(display_file, &tmp, 1);
 
-	// 	sleep(fast ? 0x400 : 0x600, fast ? 0x1000 : 0x2800);
-	// }
+		sleep(fast ? 0x400 : 0x600, fast ? 0x1000 : 0x2800);
+	}
 
-	// close(display_file);
-	// close(switch1_file);
-	// close(switch2_file);
-
-    uint32_t pipe_cv = open("SYS:cv/pipe", NFile_Open_Mode::Read_Write);
-    mutex_t pipe_mutex = mutex_create("pipe");
-    uint32_t pipe_p = pipe("com_pipe", 10);
-
-    sleep(10000);
-
-    mutex_lock(pipe_mutex);
-    write(pipe_p, "abcde", 6);
-    mutex_unlock(pipe_mutex);
-    notify(pipe_cv);
-
-    close(pipe_p);
-    close(pipe_cv);
-    mutex_destroy(pipe_mutex);
+	close(display_file);
+	close(switch1_file);
+	close(switch2_file);
 
     return 0;
 }
--- sources/userspace/tilt_task/main.cpp
+++ sources/userspace/tilt_task/main.cpp
@@ -20,85 +20,49 @@ static void fputs(uint32_t file, const char* string)
 
 int main(int argc, char** argv)
 {
-	// char state = '0';
-	// char oldstate = '0';
+	char state = '0';
+	char oldstate = '0';
 
-	// uint32_t tiltsensor_file = open("DEV:gpio/23", NFile_Open_Mode::Read_Only);
-	// // TODO: otevrit PWM
+	uint32_t tiltsensor_file = open("DEV:gpio/23", NFile_Open_Mode::Read_Only);
+	// TODO: otevrit PWM
 
-	// NGPIO_Interrupt_Type irtype;
+	NGPIO_Interrupt_Type irtype;
 	
-	// //irtype = NGPIO_Interrupt_Type::Rising_Edge;
-	// //ioctl(tiltsensor_file, NIOCtl_Operation::Enable_Event_Detection, &irtype);
+	irtype = NGPIO_Interrupt_Type::Rising_Edge;
+	ioctl(tiltsensor_file, NIOCtl_Operation::Enable_Event_Detection, &irtype);
 
-	// irtype = NGPIO_Interrupt_Type::Falling_Edge;
-	// ioctl(tiltsensor_file, NIOCtl_Operation::Enable_Event_Detection, &irtype);
+	irtype = NGPIO_Interrupt_Type::Falling_Edge;
+	ioctl(tiltsensor_file, NIOCtl_Operation::Enable_Event_Detection, &irtype);
 
-	// uint32_t logpipe = pipe("log", 32);
+	uint32_t logpipe = pipe("log", 32);
 
-	// while (true)
-	// {
-	// 	wait(tiltsensor_file, 0x800);
+	while (true)
+	{
+		wait(tiltsensor_file, 0x800);
 
-	// 	// "debounce" - tilt senzor bude chvili flappovat mezi vysokou a nizkou urovni
-	// 	//sleep(0x100, Deadline_Unchanged);
+		// "debounce" - tilt senzor bude chvili flappovat mezi vysokou a nizkou urovni
+		//sleep(0x100, Deadline_Unchanged);
 
-	// 	read(tiltsensor_file, &state, 1);
+		read(tiltsensor_file, &state, 1);
 
-	// 	//if (state != oldstate)
-	// 	{
-	// 		if (state == '0')
-	// 		{
-	// 			write(logpipe, "Tilt UP", 7);
-	// 		}
-	// 		else
-	// 		{
-	// 			write(logpipe, "Tilt DOWN", 10);
-	// 		}
-	// 		oldstate = state;
-	// 	}
+		if (state != oldstate)
+		{
+			if (state == '0')
+			{
+				write(logpipe, "Tilt UP", 7);
+			}
+			else
+			{
+				write(logpipe, "Tilt DOWN", 10);
+			}
+			oldstate = state;
+		}
 
-	// 	sleep(0x1000, Indefinite/*0x100*/);
-	// }
+		sleep(0x1000, Indefinite/*0x100*/);
+	}
 
-	// // TODO zavrit PWM
-	// close(tiltsensor_file);
-
-    uint32_t uart_file = open("DEV:uart/0", NFile_Open_Mode::Write_Only);
-
-	TUART_IOCtl_Params params;
-	params.baud_rate = NUART_Baud_Rate::BR_115200;
-	params.char_length = NUART_Char_Length::Char_8;
-	ioctl(uart_file, NIOCtl_Operation::Set_Params, &params);
-
-    uint32_t pipe_cv = open("SYS:cv/pipe", NFile_Open_Mode::Read_Write);
-    mutex_t pipe_mutex = mutex_create("pipe");
-    uint32_t pipe_p = pipe("com_pipe", 10);
-
-    char buf[16];
-    char read_b[16];
-    memset(buf, '\0', 16);
-
-    mutex_lock(pipe_mutex);
-    uint32_t read_bytes;
-    while ((read_bytes = read(pipe_p, buf, 5)) < 5)
-    {
-        fputs(uart_file, "Waiting for something to appear in pipe!\n");
-        NSWI_Result_Code code =  wait(pipe_cv);
-    }
-    mutex_unlock(pipe_mutex);
-
-    itoa(read_bytes, read_b, 10);
-    fputs(uart_file, "Read bytes: ");
-    fputs(uart_file, read_b);
-    fputs(uart_file, "\n");
-    fputs(uart_file, buf);
-    fputs(uart_file, "\n");
-
-    close(uart_file);
-    close(pipe_p);
-    close(pipe_cv);
-    mutex_destroy(pipe_mutex);
+	// TODO zavrit PWM
+	close(tiltsensor_file);
 
     return 0;
 }
